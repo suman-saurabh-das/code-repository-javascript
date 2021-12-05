f8382724d8b03d2ffb053ef5a9ab24b7f8ad3c7d
Suman Saurabh Das <dsumansaurabh@gmail.com>
2021-12-05 16:43:08 +0530

    Started Codevolution JS (ES6) & TS course
    Added 01. Intro to JavaScript ES6 and TypeScript

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # JAVASCRIPT
 
 - A comprehensive JavaScript repository containing code from multiple sources, covering fundamentals, advanced concepts, and Data Structures & Algorithms (DSA). Designed as a centralized knowledge base for learning, practicing, and mastering JavaScript.
+
+### Courses
+
+- [JavaScript (ES6) & TypeScript - Codevolution](https://youtube.com/playlist?list=PLC3y8-rFHvwhI0V5mE9Vu6Nm-nap8EcjV&si=Vh7KQFbfANOJZ0KU)
---
+++ js-and-ts-codevolution/01. Intro to JavaScript ES6 and TypeScript.ts
@@ -0,0 +1,32 @@
+/**
+ * 01. INTRODUCTION TO JAVASCRIPT ES6 & TYPESCRIPT
+ *
+ * • ES6 (ECMAScript 2015)
+ *  - Major update to JavaScript.
+ *  - Introduced modern features like let/const, arrow functions,
+ *    classes, modules, promises, destructuring, etc.
+ *
+ * • TypeScript
+ *  - Open-source language developed by Microsoft.
+ *  - Typed superset of JavaScript.
+ *  - Compiles to JavaScript.
+ *  - Supports latest ECMAScript features.
+ *  - Adds static typing, interfaces, generics, etc.
+ *  - Types help in writing safer and scalable code.
+ *
+ * 02. TRANSPILER
+ *
+ * • A transpiler converts modern JavaScript or TypeScript into backward-compatible JavaScript.
+ * • Examples: Babel, TypeScript (tsc), esbuild / SWC (modern tools)
+ *
+ * 03. ENVIRONMENT SETUP
+ *
+ * • VS code: https://code.visualstudio.com/
+ * • Node.JS: https://nodejs.org/en
+ *
+ * • Install TypeScript compiler: npm install typescript
+ * • Install TypeScript file runner: npm install tsx
+ * • Run using: npx tsx npx tsx 01.\ Intro\ to\ JavaScript\ ES6\ and\ TypeScript.ts
+ */
+
+console.log("Introduction to JS (ES6) & TS");

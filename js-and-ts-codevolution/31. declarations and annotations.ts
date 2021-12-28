ab549d934c6386c48630ccb4228f56921c8c22b8
Suman Saurabh Das <dsumansaurabh@gmail.com>
2021-12-28 17:41:16 +0530

    Added 31. declarations and annotations

---
+++ js-and-ts-codevolution/31. declarations and annotations.ts
@@ -0,0 +1,23 @@
+/**
+ * 01. TYPESCRIPT
+ *
+ * • Typed superset of JavaScript that compiles to plain JavaScript.
+ * • Types are optional (specifying the datatypes is optional).
+ *
+ * • Why learn TypeScript ?
+ *  - Identifies mistakes during development.
+ *  - Tool support.
+ *  - TS = ES5 + ES6 + ES7 + Features
+ */
+
+/** 02. DECLARATIONS & ANNOTATIONS */
+
+// • Type inference: TS determines the type of variable automatically.
+var numberVariable = 13141190;
+var stringVariable = "Saurabh";
+var booleanVariable = true;
+
+// • Explicit declaration of variable using annotation.
+// • Initialization of variable is not mandatory to infer type of variable.
+var numberVariable2: number = 13141190;
+var stringVariable2: string;
